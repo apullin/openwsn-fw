51ffae811f7d427e70e4ebff5a273bcdc2b208c6
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2011-09-08 18:56:24 +0000

    mote prints schedule

--- openwsn/02b-RES/schedule.h
+++ openwsn/02b-RES/schedule.h
@@ -23,22 +23,22 @@ enum {
 
 //=========================== typedef =========================================
 
-typedef uint8_t   cellType_t;
-typedef uint8_t   channelOffset_t;
+typedef uint8_t    cellType_t;
+typedef uint8_t    channelOffset_t;
 
 typedef struct {
-   uint8_t        type;
-   uint8_t        channelOffset;
-   open_addr_t    neighbor;
-   uint8_t        numUsed;
-   uint8_t        numTxACK;
-   timervalue_t   timestamp;
-} cellUsageInformation_t;
+   uint8_t         type;
+   uint8_t         channelOffset;
+   open_addr_t     neighbor;
+   uint8_t         numUsed;
+   uint8_t         numTxACK;
+   timervalue_t    timestamp;
+} scheduleRow_t;
 
 typedef struct {
-   uint8_t                row;
-   cellUsageInformation_t cellUsage;
-} debugCellUsageInformation_t;
+   uint8_t         row;
+   scheduleRow_t   cellUsage;
+} debugScheduleRow_t;
 
 //=========================== variables =======================================
 
@@ -49,7 +49,6 @@ typedef struct {
 __monitor cellType_t      schedule_getType(asn_t asn_param);
 __monitor channelOffset_t schedule_getChannelOffset(asn_t asn_param);
 __monitor void            schedule_getNeighbor(asn_t asn_param, open_addr_t* addrToWrite);
-          bool            schedule_debugPrint();
 
 /**
 \}
