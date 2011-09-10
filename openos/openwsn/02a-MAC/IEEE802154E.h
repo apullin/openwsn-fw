4c5fc834d6d90e2ab5783274f64fdee30cfbeedb
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2011-09-10 00:30:13 +0000

    typos in mote code

--- openos/openwsn/02a-MAC/IEEE802154E.h
+++ openos/openwsn/02a-MAC/IEEE802154E.h
@@ -125,7 +125,7 @@ __monitor asn_t ieee154e_getAsn();
 // misc
           bool  debugPrint_asn();
           bool  debugPrint_isSync();
-          bool  debugPrint_statsMac();
+          bool  debugPrint_macStats();
 
 /**
 \}
