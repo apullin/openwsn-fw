409e6f9bd6bfdda97ae0aed8a5839b605b336e88
Thomas Watteyne <watteyne@eecs.berkeley.edu>
2011-09-13 18:33:11 +0000

    DEBUG_MOTEID_MASTER is DAGroot and bridge

--- openos/openwsn/03a-IPHC/openbridge.c
+++ openos/openwsn/03a-IPHC/openbridge.c
@@ -49,8 +49,8 @@ void openbridge_sendDone(OpenQueueEntry_t* msg, error_t error) {
    msg->owner = COMPONENT_OPENBRIDGE;
    if (msg->creator!=COMPONENT_OPENBRIDGE) {
       openserial_printError(COMPONENT_OPENBRIDGE,ERR_UNEXPECTED_SENDDONE,
-            (errorparameter_t)0,
-            (errorparameter_t)0);
+                            (errorparameter_t)0,
+                            (errorparameter_t)0);
    }
    openqueue_freePacketBuffer(msg);
 }
