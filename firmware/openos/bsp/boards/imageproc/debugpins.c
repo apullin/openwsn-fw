255dea0f051b9ae20d44e76dff451a709bf032b0
Andrew Pullin <pullin@berkeley.edu>
2013-02-06 10:02:51 -0800

    Correcting order and layout of debug pins.

--- firmware/openos/bsp/boards/imageproc/debugpins.c
+++ firmware/openos/bsp/boards/imageproc/debugpins.c
@@ -18,12 +18,12 @@
 
 void debugpins_init() {
     //Debug pins on camera connector
-    TRISDbits.TRISD0 = 0;
-    TRISDbits.TRISD1 = 0;
+    TRISFbits.TRISF1 = 0;
     TRISDbits.TRISD2 = 0;
+    TRISDbits.TRISD1 = 0;
     TRISDbits.TRISD3 = 0;
+    TRISDbits.TRISD0 = 0;
     TRISDbits.TRISD4 = 0;
-    TRISDbits.TRISD5 = 0;
 }
 
 // RD2
@@ -39,69 +39,69 @@ void debugpins_frame_set() {
     _RD2 = 0x1;
 }
 
-// RD3
-void debugpins_slot_toggle() {
-    _RD3 ^= 0x1;
-}
-
-void debugpins_slot_clr() {
-    _RD3 = 0x0;
-}
-
-void debugpins_slot_set() {
-    _RD3 = 0x1;
-}
-
-// RD4
-void debugpins_fsm_toggle() {
-    _RD4 ^= 0x1;
-}
-
-void debugpins_fsm_clr() {
-    _RD4 = 0x0;
-}
-
-void debugpins_fsm_set() {
-    _RD4 = 0x1;
-}
-
-// RD5
-void debugpins_task_toggle() {
-    _RD5 ^= 0x1;
-}
-
-void debugpins_task_clr() {
-    _RD5 = 0x0;
-}
-
-void debugpins_task_set() {
-    _RD5 = 0x1;
-}
-
-// RD0
-void debugpins_isr_toggle() {
-    _RD0 ^= 0x1;
-}
-
-void debugpins_isr_clr() {
-    _RD0 = 0x0;
-}
-
-void debugpins_isr_set() {
-    _RD0 = 0x1;
-}
-
 // RD1
-void debugpins_radio_toggle() {
+void debugpins_slot_toggle() {
     _RD1 ^= 0x1;
 }
 
-void debugpins_radio_clr() {
+void debugpins_slot_clr() {
     _RD1 = 0x0;
 }
 
-void debugpins_radio_set() {
+void debugpins_slot_set() {
     _RD1 = 0x1;
+}
+
+// RD3
+void debugpins_fsm_toggle() {
+    _RD3 ^= 0x1;
+}
+
+void debugpins_fsm_clr() {
+    _RD3 = 0x0;
+}
+
+void debugpins_fsm_set() {
+    _RD3 = 0x1;
+}
+
+// RD0
+void debugpins_task_toggle() {
+    _RD0 ^= 0x1;
+}
+
+void debugpins_task_clr() {
+    _RD0 = 0x0;
+}
+
+void debugpins_task_set() {
+    _RD0 = 0x1;
+}
+
+// RD4
+void debugpins_isr_toggle() {
+    _RD4 ^= 0x1;
+}
+
+void debugpins_isr_clr() {
+    _RD4 = 0x0;
+}
+
+void debugpins_isr_set() {
+    _RD4 = 0x1;
+}
+
+// RF1
+void debugpins_radio_toggle() {
+    _RF1 ^= 0x1;
+}
+
+void debugpins_radio_clr() {
+    _RF1 = 0x0;
+}
+
+void debugpins_radio_set() {
+    _RF1 = 0x1;
 }
 
 //=========================== private =========================================
